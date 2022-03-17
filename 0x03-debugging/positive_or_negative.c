a5daac0c9aefcc9ce25ad3e3899d49389cc2a077
AlainPascal <ialainpascal@gmail.com>
2022-03-17 14:55:14 -0700

    Push untracked file

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - positive anything is better than negative nothing
+ * @i: number to be checked
+ *
+ * Description: This program will prints whether the number stored in the
+ * variable i is positive or negative
+ *
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	} else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	} else
+	{
+		printf("%d is zero\n", i);
+	}
+}
