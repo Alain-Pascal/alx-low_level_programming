2fcfa43591beaff5c87ab76e83647aa0f92bb895
AlainPascal <ialainpascal@gmail.com>
2022-03-19 10:57:34 -0700

    Write function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# 0x04-more_functions_nested_loops
+
+This directory contains some C files about more functions, nested loops
+
+## files
+
+* main.h
+* 0-isupper.c
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prototype of function _putchar */
+int _putchar(char c);
+
+/* protype of function _isupper */
+int _isupper(int c);
+
+#endif /* MAIN_H */
