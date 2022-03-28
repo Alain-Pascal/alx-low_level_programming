c719919976a810740340a73d8624aaf01f0521ce
AlainPascal <ialainpascal@gmail.com>
2022-03-28 14:41:28 -0700

    Write function that locates a charachter in a string

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+# 0x07-pointers_arrays_strings
+
+This directory contains some C files about pointers, arrays, and strings
+
+## files
+
+* main.h
+* _putchar.c
+* 2-strchr.c
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prototype of functin _putchar */
+int _putchar(char c);
+
+/* prototype of function _strchr */
+char *_strchr(char *s, char c);
+
+#endif /* MAIN_H */
