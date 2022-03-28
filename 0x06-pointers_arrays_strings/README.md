c7dbf8763938121383064754826738dedec2bc6c
AlainPascal <ialainpascal@gmail.com>
2022-03-28 13:52:39 -0700

    Write function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+# 0x06-pointers_arrays_strings
+
+This directory contains some C files about pointers, arrays, and strings
+
+## files
+
+* main.h
+* _putchar.c
+* 0-strcat.c
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prototype of functin _putchar */
+int _putchar(char c);
+
+/* prototype of function _strcat */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
