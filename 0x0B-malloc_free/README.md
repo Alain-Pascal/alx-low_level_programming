1421139166f0d72f4bcfab682de18eed858369bc
AlainPascal <ialainpascal@gmail.com>
2022-04-28 12:07:48 -0700

    Write function that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+# 0x0B-malloc_free
+
+This directory contains C files about mallloc, free
+## files
+
+* main.h
+* 0-create_array.c
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* prototype of function _putchar */
+int _putchar(char c);
+
+/* prorotype of function create_array */
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
