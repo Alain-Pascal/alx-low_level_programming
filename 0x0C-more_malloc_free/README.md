692a2edcc52db7cbef90013067493190ebd35cba
AlainPascal <ialainpascal@gmail.com>
2022-04-29 02:42:54 -0700

    Write function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,8 @@
+# 0x0C-more_malloc_free
+
+This directory contains more C files about mallloc, free
+
+## files
+
+* main.h
+* 0-malloc_checked.c
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/* prototype of function _putchar */
+int _putchar(char c);
+
+/* prototype of function malloc_checked */
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
