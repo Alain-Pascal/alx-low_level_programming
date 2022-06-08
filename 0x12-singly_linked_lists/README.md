044b405fa2b4cdadb8d553b6ec35b98827a41d41
AlainPascal <ialainpascal@gmail.com>
2022-06-08 12:11:51 +0300

    Write 100-first.c function file

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -12,3 +12,4 @@ project
 * 2-add_node.c
 * 3-add_node_end.c
 * 4-free_list.c
+* 100-first.c
---
+++ 0x12-singly_linked_lists/testFiles/100-main.c
@@ -0,0 +1,14 @@
+#include "../lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, ");
+	printf("challenges one to a race.)\n");
+
+	return (0);
+}
