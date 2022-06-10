095e2611254833f435341e9636cc2d6693ae24e9
AlainPascal <ialainpascal@gmail.com>
2022-06-11 00:01:40 +0300

    Write function that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -9,3 +9,4 @@ This directory contains some C files about 0x13. C - More singly linked lists pr
 * 0-print_listint.c
 * 1-listint_len.c
 * 2-add_nodeint.c
+* 3-add_nodeint_end.c
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,9 +8,8 @@
 #include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -28,5 +27,7 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
 listint_t *add_nodeint(listint_t **head, const int n);
+
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif /* LISTS_H */
