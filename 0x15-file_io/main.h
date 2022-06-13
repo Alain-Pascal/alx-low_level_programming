44eb08b47278fe5d44a9bb3fda289b8f5850325e
AlainPascal <ialainpascal@gmail.com>
2022-06-13 14:30:26 +0300

    Write program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,6 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void file_error(int f_from, int f_to, char *av[]);
+
 #endif /* MAIN_H */
