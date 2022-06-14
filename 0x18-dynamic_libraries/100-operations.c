65cac7be9c3cdcf6ae10e3387c2b709c67fd5100
AlainPascal <ialainpascal@gmail.com>
2022-06-14 03:41:58 +0300

    Add 100-operations.c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result from subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: integer
+ * @b: integer
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: integer
+ * @b: integer
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: integer
+ * @b: integer
+ *
+ * Return: modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
