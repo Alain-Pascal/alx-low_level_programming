2d5d916afa7ecc462f7439593948bcd10b2acd37
Alain Pascal <ialainpascal@gmail.com>
2025-04-16 03:37:26 +0300

    Betty coding style

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,11 +4,12 @@
  * set_string -sets the value of a pointer to a char.
  * @s: Double pointer to a char
  * @to: Pointer to the char to set s to
- * 
+ *
  * Description: This function takes a double pointer 's' and an pointer 'to'
  * It modifies the value pointed to by 's' to be the address held by 'to'
  */
 
-void set_string(char **s, char *to){
+void set_string(char **s, char *to)
+{
 	*s = to;
 }
